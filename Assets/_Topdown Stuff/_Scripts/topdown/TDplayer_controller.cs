70c67032e32293c8b11fe837fa451c326b2d7505
jarab310 <jar310@pitt.edu>
2023-12-14 22:32:40 -0500

    Revert "made interact use the Fire2 input (e and x)"
    This reverts commit fd3a64971346369c1ca9dd4d74865994e96805c6.

--- Assets/_Topdown Stuff/_Scripts/topdown/TDplayer_controller.cs
+++ Assets/_Topdown Stuff/_Scripts/topdown/TDplayer_controller.cs
@@ -29,7 +29,7 @@ public class TDplayer_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Fire2") > 0) 
+        if (Input.GetKeyDown(KeyCode.E)) 
         { 
             Checkinteraction();
         }
