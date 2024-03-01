22d6cdb22cd1b4dc97d9056b91fa6e5d9766d782
khailil <kht10@pitt.edu>
2024-03-01 14:32:03 -0500

    new topdown dialogue stuff

--- Assets/_Topdown Stuff/_Scripts/LockedOpenable.cs
+++ Assets/_Topdown Stuff/_Scripts/LockedOpenable.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LockedOpenable : Interactable
 {
@@ -13,10 +14,19 @@ public class LockedOpenable : Interactable
     public int keysHeld;
     public GameObject ah;
     public GameObject yessir;
-    public GameObject text;
-    public Vector3 textVec;
+ 
     public GameObject e;
 
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public Text NPCname;
+    public string yer;
+    public string[] dialogue;
+    private int index;
+
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool isPlayerClose;
     public override void Interact()
     {
         if (keysHeld == keysReq)
@@ -32,11 +42,57 @@ public class LockedOpenable : Interactable
         }
         else if (keysHeld != keysReq && sr.sprite == closed) 
         {
-            textVec.x = player.transform.position.x;
-            textVec.z = player.transform.position.z;
-            textVec.y = player.transform.position.y - 4;
-            Instantiate(text, textVec, player.transform.rotation);
-            player.GetComponent<TDplayer_controller>().canMove = false;
+            if (Input.GetKeyDown(KeyCode.E) && isPlayerClose)
+            {
+                if (dialoguePanel.activeInHierarchy)
+                {
+                    ZeroText();
+                }
+                else
+                {
+                    dialoguePanel.SetActive(true);
+                    StartCoroutine(Typing());
+                }
+
+            }
+        }
+
+        if (dialogueText.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+        }
+    }
+
+    public void ZeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+
+    }
+
+    public void NextLine()
+    {
+
+        contButton.SetActive(false);
+        if (index < dialogue.Length)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ZeroText();
         }
     }
 
@@ -59,4 +115,20 @@ public class LockedOpenable : Interactable
         sr.sortingLayerID = default;
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerClose = false;
+            ZeroText();
+        }
+    }
 }
--- Assets/villager.cs
+++ Assets/villager.cs
@@ -1,29 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class villager : Interactable
+public class NPC : MonoBehaviour
 {
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public Text NPCname;
+    public string yer;
+    public string[] dialogue;
+    private int index;
 
 
-    public GameObject player;
-    public GameObject text;
-    public Vector3 textVec;
 
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool isPlayerClose;
 
-    public override void Interact()
+    // Update is called once per frame
+    private void Start()
     {
-        textVec.x = player.transform.position.x;
-        textVec.z = player.transform.position.z;
-        textVec.y = player.transform.position.y - 4;
-        Instantiate(text, textVec, player.transform.rotation);
+        NPCname.text = yer;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerClose) 
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                ZeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+
+        }
+
+        if(dialogueText.text == dialogue[index])
+        {
+            contButton.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                NextLine();
+            }
+        }
+
+        
+    }
+
+    public void NextLine()
+    {
+
+        contButton.SetActive(false);
+        if(index < dialogue.Length)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ZeroText();
+        }
+    }
+
+    public void ZeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
 
     }
 
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            isPlayerClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerClose = false;
+            ZeroText();
+        }
     }
 }
