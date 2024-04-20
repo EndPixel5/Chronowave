4d9c1dea4276e1add2a60f7424c47d1c77b50ee5
jarab310 <jar310@pitt.edu>
2024-04-19 20:57:44 -0400

    boss charge be working

--- Assets/bigIronBaseScript.cs
+++ Assets/bigIronBaseScript.cs
@@ -13,6 +13,7 @@ public class bigIronBaseScript : MonoBehaviour
     public Rigidbody2D body;
     public float attackTimer = -1;
     public float attackChoice = 0;
+    public bool justBeganCharge = false;
    // private bool allowPrevious = false;
     private float prev = -100;
 
@@ -37,7 +38,7 @@ public class bigIronBaseScript : MonoBehaviour
             attackTimer--;
 
         }
-        else
+        else if (attackTimer == -1)
         {
             if (attackChoice == 0)
             {
@@ -68,10 +69,24 @@ public class bigIronBaseScript : MonoBehaviour
     private void charge()
     {
         body.velocity = transform.right * speed;
-
+        justBeganCharge = true;
+        attackTimer = -2;
         //allowPrevious = true;       
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "WAWAWAWA")
+        {
+            if (justBeganCharge == true) { justBeganCharge = false; }
+            else
+            {
+                body.velocity *= 0;
+                transform.Rotate(0, 180, 0);
+                attackTimer = 45;
+            }
+        }
+    }
     /*private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Platform")
