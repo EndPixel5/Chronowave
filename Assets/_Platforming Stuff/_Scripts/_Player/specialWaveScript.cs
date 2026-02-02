9e63a217c01bac0baa52ede24b3fc94e28d3e762
jarab310 <jar310@pitt.edu>
2026-02-02 16:03:05 -0500

    Revert "THE WAVE"
    This reverts commit 170ac26f14c881a928f8fc674e9e76cf2c4c86ae.

--- Assets/_Platforming Stuff/_Scripts/_Player/specialWaveScript.cs
+++ Assets/_Platforming Stuff/_Scripts/_Player/specialWaveScript.cs
@@ -5,82 +5,32 @@ using UnityEngine;
 public class specialWaveScript : MonoBehaviour
 {
     public bool specialAvailable = false;
-    //public GameObject trailPrefab;
-    //public float speed;
-    //public Rigidbody2D body;
-
-
-
-    [SerializeField] float moveSpeed = .05f;
-    [SerializeField] float frequency = 1;
-    [SerializeField] float magnitude = .05f;
-    private bool facingRight = true;
-    Vector3 pos, localScale;
-    [SerializeField] int mids = 0;
+    public GameObject trailPrefab;
+    public float speed;
+    public Rigidbody2D body;
    // public bool specialAvailable = false;
     // Start is called before the first frame update
     void Start()
     {
-        pos = transform.position;
-        localScale = transform.localScale;
-        //body.velocity = transform.up * speed;
+        body.velocity = transform.up * speed;
     }
 
-    private void Update()
-    {
-        if(facingRight)
-        {
-            moveRight();
-        }
-        else
-        {
-            moveLeft();
-        }
-    }
-
-    private void moveRight()
-    {
-        pos += transform.right * Time.deltaTime * moveSpeed;
-        transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
-    }
-
-    private void moveLeft()
-    {
-        pos -= transform.right * Time.deltaTime * moveSpeed;
-        transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
-    }
-
-    /*void FixedUpdate()
+    void FixedUpdate()
     {
         Instantiate(trailPrefab, gameObject.transform.position, gameObject.transform.rotation);
-    }*/
-
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        /*if (collision.gameObject.tag == "key")
+        if (collision.gameObject.tag == "key")
         {
             transform.Rotate(180, 0, 0);
             body.velocity = transform.up * speed;
-        }*/
+        }
         if(collision.gameObject.tag == "part")
         {
             specialAvailable = true;
-            mids++;
         }
-        if(mids == 4)
-        {
-            mids = 1;
-            facingRight = !facingRight;
-            localScale.x *= -1;
-        }
-        /*else if(collision.gameObject.tag == "key")
-        {
-            facingRight = !facingRight;
-            if((facingRight && localScale.x <0) || !facingRight && localScale.x>0)
-            {
-                localScale.x *= -1;
-            }
-        }*/
     }
 
     private void OnTriggerExit2D(Collider2D collision)
